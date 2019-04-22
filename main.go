4a7506f93cdfedca072ef8c6ec5b306743d8a8d1
meowmeowhappykitten <broccoleigh@gmail.com>
2019-04-21 22:37:51 -0700

    reading yml file of chores

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ type people struct {
 	People []string `yaml:"people"`
 }
 
-func (p *people)readinputpeople(file string) *people {
+type chore struct {
+	Description string `yaml:"description"`
+	Frequency   string `yaml:"frequency"`
+}
+
+type chores struct {
+	Chores []map[string]chore
+}
+
+func (p *people) readinputpeople(file string) *people {
 	peopleFile, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println("Error reading people file:", err)
@@ -24,9 +33,26 @@ func (p *people)readinputpeople(file string) *people {
 
 }
 
+func (c *chores) readinputchores(file string) *chores {
+	choreFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error reading chores file:", err)
+	}
+	err = yaml.Unmarshal(choreFile, c)
+	if err != nil {
+		fmt.Println("Error Unmarshalling chores file:", err)
+	}
+
+	return c
+
+}
+
 func main() {
-  var p people
+	var p people
+	var c chores
 	p.readinputpeople("settings/people.yml")
+	c.readinputchores("settings/chores.yml")
 	fmt.Println(p)
+	fmt.Println(c)
 
 }
