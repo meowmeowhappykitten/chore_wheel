442150d42779327f81cfa10a48457844e8e1654b
meowmeowhappykitten <broccoleigh@gmail.com>
2019-04-21 21:12:38 -0700

    reading yaml file of people

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
+type people struct {
+	People []string `yaml:"people"`
+}
+
+func (p *people)readinputpeople(file string) *people {
+	peopleFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error reading people file:", err)
+	}
+	err = yaml.Unmarshal(peopleFile, p)
+	if err != nil {
+		fmt.Println("Error Unmarshalling people file:", err)
+	}
+
+	return p
+
+}
+
 func main() {
-	fmt.Println("Initial commit.")
+  var p people
+	p.readinputpeople("settings/people.yml")
+	fmt.Println(p)
+
 }
